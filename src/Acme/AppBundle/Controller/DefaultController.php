81a3558c0ce1d1b489df985a959f6ce9fad5d13b
Ecole <Ecole>
2016-03-08 12:01:59 +0100

    add list on index

--- src/Acme/AppBundle/Controller/DefaultController.php
+++ src/Acme/AppBundle/Controller/DefaultController.php
@@ -4,14 +4,34 @@ namespace Acme\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
+     * @Method({"GET","HEAD"})
+     *
      */
-    public function indexAction()
-    {
-        return $this->render('AcmeAppBundle:Default:index.html.twig');
+    public function indexAction(){
+        $items = [
+            [
+                'name' => "Google",
+                'url' => 'http://google.fr',
+                'created' => new \DateTime()
+            ],
+            [
+                'name' => "Facebook",
+                'url' => 'http://facebook.fr',
+                'created' => new \DateTime()
+            ],
+            [
+                'name' => "Twitter",
+                'url' => 'http://twitter.com',
+                'created' => new \DateTime()
+            ],
+        ];
+
+        return $this->render('AcmeAppBundle:Default:index.html.twig', ['links' => $items]);
     }
 }
